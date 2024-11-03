0defa73bd6884ffdd0f6bdb0c4a703243543e3ae
cepheus101 <cepheus2005@gmail.com>
2024-11-03 23:47:05 +0530

    update

--- palindrome.f90
+++ palindrome.f90
@@ -1,19 +1,25 @@
 program palindrome
 implicit none
-integer :: n,i,l,rev
+integer :: n,i,l,rev,rem,temp
 l=0
 i=1
 rev=0
 print *, 'Enter a Number:'
 read(*,*) n
-do while(n>0)
-    n=n/10
-    l=l+1
+temp=n
+!do while(n>0)
+ !   temp=temp/10
+  !  l=l+1
+!end do
+do while(temp>0)
+    rem=mod(temp,10)
+    rev = (rev * 10) + rem; 
+    temp = temp / 10;
 end do
-do while(i<=l)
-    
-    
-    
-end do
-print *, rev 
+print *, 'Reverse of the Number=',rev
+if(rev==n) then
+    print *, 'The number is palindrome'
+else
+    print *, 'The number is not palindrome'
+end if
 end program palindrome
