714cfc60c9c694847e23f3f8249ba31a592a6f50
cepheus101 <cepheus2005@gmail.com>
2024-11-21 12:56:00 +0530

    update

--- multiply_array.f90
+++ multiply_array.f90
@@ -1,15 +1,41 @@
-program multiply_array
 implicit none
-integer,dimension(2,2) :: A
-integer,dimension(2,2) :: B
-integer,dimension(2,2) :: C
-integer :: i,j
-A=[1,2,3,4]
-B=[1,2,3,4]
-do i=1,2
-    do j=1,2
-        C(i,j)=A(i)*B(j)+A(i+1)*B(j+1)
+integer,dimension(:,:),allocatable::a,b,c
+integer i,j,k,l,m,n,o
+5 print*,'give dimension a'
+read(*,*)k,l
+print*,'give dimension b'
+read(*,*)m,n
+if (m/=l)then
+    print*,'wrong input'
+    goto 5
+end if
+allocate(a(k,l),b(m,n),c(k,n))
+print*,'give values for a '
+do i=1,k
+    do j=1,l
+        read(*,*) A(i,j)
+end do
+end do
+print*,'give values for b'
+do i=1,m
+    do j=1,n
+        read(*,*) b(i,j)
+end do
+end do
+do i=1,k
+    do j=1,n
+        c(i,j)=0
+end do
+end do
+do i=1,k
+    do j=1,n
+        do o=1,l
+            c(i,j)=(a(i,o)*b(o,j))+c(i,j)
+        end do 
     end do
 end do
-do i=
-end program multiply_array
+print *, 'Multiplication of two given array '
+do i=1,k
+        print*,(c(i,j),j=1,n)
+end do
+end program 
